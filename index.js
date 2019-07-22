292fae7b2080749fe4f930d4b4795c7121ce6788
Vitaliy Filipov <altras@gmail.com>
2019-07-22 21:52:15 +0300

    Fixes relative path to dna from node-modules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // This tells the 'config' library to get it's config from the dna folder.
-process.env.NODE_CONFIG_DIR = '../dna'
+process.env.NODE_CONFIG_DIR = '../../dna'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 /*
 
